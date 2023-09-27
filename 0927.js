b635b2c312894ffb4526d60524aa46a4870faf0e
iodhfis <133744303+iodhfis@users.noreply.github.com>
2023-09-27 16:34:26 +0900

    230927 실습

---
+++ 0927.js
@@ -0,0 +1,33 @@
+console.log('my' + 'cat');
+console.log('1' + 2);
+console.log(`string literals: 1 + 2 = ${1 + 2}`);
+
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(1 / 1);  
+console.log(1 * 1); 
+console.log(5 % 2); 
+console.log(2 ** 3);
+
+console.log(10 < 6);
+console.log(10 <= 6);
+console.log(10 > 6);
+console.log(10 >= 6);
+
+const value1 = false;
+const value2 = 4 < 2;
+
+console.log(value1 || value2);
+
+console.log(value1 && value2);
+
+console.log(!value1);
+
+const stringFive = '5';
+const numberFive = 5;
+
+console.log(stringFive == numberFive);
+console.log(stringFive != numberFive);
+
+console.log(stringFive === numberFive);
+console.log(stringFive !== numberFive);
